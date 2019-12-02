656d3a63b371102c19ad15e58610014242cdf8cd
hankail05 <39765074+hankail05@users.noreply.github.com>
2019-12-02 23:37:38 +0900

    delay loading snippets

--- .emacs.d/config/utils/init-yasnippet.el
+++ .emacs.d/config/utils/init-yasnippet.el
@@ -5,10 +5,16 @@
 
 (leaf yasnippet
   :straight t
-  :hook ((prog-mode-hook org-mode-hook) . yas-minor-mode)
-  :config (progn (yas-reload-all)
-                 (evil-leader/set-key
+  :hook (((prog-mode-hook org-mode-hook) . yas-minor-mode)
+         (yas-minor-mode-hook . yas-reload-all))
+  :config (progn (evil-leader/set-key
                    "y" 'yas-insert-snippet)))
+
+
+;;; snippets
+
+(leaf yasnippet-snippets
+  :straight t)
 
 
 ;;; init-yasnippet ends here
