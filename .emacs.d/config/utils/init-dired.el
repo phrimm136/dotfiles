2353953795f789eca7226089e176eb1c97593c3d
hankail05 <39765074+hankail05@users.noreply.github.com>
2020-01-07 21:42:05 +0900

    Colorful, categorized dired

--- .emacs.d/config/utils/init-dired.el
+++ .emacs.d/config/utils/init-dired.el
@@ -3,15 +3,53 @@
 ;;; Code:
 
 
-(leaf dired-subtree
+;;; more readable dired
+
+(setq dired-listing-switches "-aBlhv")
+
+
+;;; filtering and sorting files and subdirectories
+
+(leaf dired-filter
   :straight t
-  :setq ((dired-subtree-line-prefix . (lambda (depth) (make-string (* 2 depth) ?\s)))
-         (dired-subtree-use-backgrounds . nil)
-         (dired-auto-revert-buffer . t))
-  :config (progn (evil-define-key 'normal dired-mode-map
-                   "gf" 'counsel-find-file
-                   "gb" 'counsel-switch-buffer
-                   (kbd "<tab>") 'dired-subtree-toggle
-                   (kbd "<backtab>") 'dired-subtree-toggle)))
+  :hook ((dired-mode-hook . dired-filter-group-mode))
+  :setq ((dired-filter-group-saved-groups . '(("default"
+                                               ("Code"
+                                                (extension "py" "ipynb" "rb" "pl" "t" "msql" "mysql" "pgsql" "sql" "r" "clj" "cljs" "scala" "js"
+                                                           "asm" "cl" "lisp" "el" "c" "h" "c++" "h++" "hpp" "hxx" "m" "cc" "cs" "cp" "cpp" "go"
+                                                           "f" "for" "ftn" "f90" "f95" "f03" "f08" "s" "rs" "hi" "hs" "pyc" "java" "jl" "scm"
+                                                           "sh"))
+                                               ("Document"
+                                                (extension "pdf" "docx"))
+                                               ("Markdown"
+                                                (extension "org" "etx" "info" "markdown" "md" "mkd" "nfo" "pod" "rst" "tex" "textfile" "txt"))
+                                               ("Archives"
+                                                (extension "zip" "rar" "gz" "bz2" "tar")))))))
+
+
+;;; integrate ranger feature
+
+(leaf dired-ranger
+  :straight t
+  :config (progn (evil-define-key 'normal 'dired-mode-map
+                   "C" 'dired-ranger-copy
+                   "P" 'dired-ranger-paste
+                   "M" 'dired-ranger-move)))
+
+
+;;; direct access to only file/subdirectory in a directory
+
+(leaf dired-collapse
+  :straight t
+  :hook ((dired-mode-hook . dired-collapse-mode)))
+
+
+;;; show git info
+
+(leaf dired-git-info
+  :straight t
+  :hook ((dired-after-readin-hook . dired-git-info-auto-enable))
+  :setq ((dgi-auto-hide-details-p . nil)))
+
 
 ;;; init-dired.el ends here
