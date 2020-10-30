555617d10e3e4e05509bad9599bc48c4baac2915
hankail05 <39765074+hankail05@users.noreply.github.com>
2020-10-30 19:31:40 +0900

    Make miscellaneous configures run before init.el

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,32 +1,99 @@
-;;; package --- summary:
+;;; early-init.el --- touch default settings -*- lexical-binding: t -*-
+
 ;;; Commentary:
+
 ;;; Code:
 
 
 ;;; no more mouse
+
 (menu-bar-mode 0)
 (tool-bar-mode 0)
+(setq-default scroll-bar-mode nil)
+(toggle-scroll-bar nil)
 
 
-;;; transparency
+;;; Ignore .Xresources
 
-(set-frame-parameter (selected-frame) 'alpha '(80 80))
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+
+;;; only utf-8
+
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-locale-environment ".UTF-8")
+(prefer-coding-system 'utf-8)
+(setq-default utf-translate-cjk-mode nil)
 
 
 ;;; font
 
 (set-face-attribute 'default nil :family "DejaVuSansMono Nerd Font")
-(set-face-attribute 'default nil :height 60)
+(set-face-attribute 'default nil :height 100)
 (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
-(setq face-font-rescale-alist
-      '(("NanumGothicCoding" . 1.2)))
+(setq-default face-font-rescale-alist
+              '(("NanumGothicCoding" . 1.2307692307692308)))
+
+;;; just one press for yes or no
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 
-;;; misc
+;;; no tabs; only spaces
 
-(advice-add #'x-apply-session-resources :override #'ignore)
+(setq-default indent-tabs-mode nil)
 
-(setq package-enable-at-startup nil)
+
+;;; Trim trailing whitespaces when saving files.
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+
+;;; redirect backup to separate directory; not more #
+
+(setq-default backup-directory-alist '((".*" . "~/.emacs.d/backups/"))
+              auto-save-file-name-transforms '((".*" "~/.emacs.d/backups/" t)))
+
+
+;;; No issue link needed
+
+(bug-reference-mode nil)
+
+
+;;; smooth scrolling
+
+(setq-default redisplay-dont-pause t
+              scroll-margin 1
+              scroll-step 1
+              scroll-conservatively 10000
+              scroll-preserve-screen-position 1
+              mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+
+;;; disable package.el
+
+(setq-default package-enable-at-startup nil)
+
+
+;;; Truncate exceeding lines
+
+(setq truncate-lines 0)
+
+
+;;; For source files add line number indicator
+
+(dolist (editor '(prog-mode-hook org-mode-hook text-mode-hook yaml-mode-hook))
+  (add-hook editor
+            (lambda ()
+              (setq indicate-empty-lines t)
+              (setq display-line-numbers 'relative))))
+
+
+;;; Activate narrow
+
+(put 'narrow-to-region 'disabled nil)
 
 
 ;;; early-init.el ends here
