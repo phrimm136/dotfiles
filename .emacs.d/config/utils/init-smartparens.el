ddad17c12476a1d3d9c18465b28b8dfd5fba6f1e
hankail05 <39765074+hankail05@users.noreply.github.com>
2020-11-02 14:31:02 +0900

    Remove workaround of parens inserting error

--- .emacs.d/config/utils/init-smartparens.el
+++ .emacs.d/config/utils/init-smartparens.el
@@ -9,10 +9,7 @@
   :setq ((sp-ignore-modes-list . (delete 'minibuffer-inactive-mode sp-ignore-modes-list))
          (sp-escape-quotes-after-insert . nil))
   :config (progn (smartparens-global-strict-mode)
-                 (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-                 (when (version<= "27" emacs-version)
-                   (dolist (parens '(c-electric-paren c-electric-brace c-electric-slash))
-                     (add-to-list 'sp--special-self-insert-commands parens)))))
+                 (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)))
 
 
 ;;; evil-mode integration
