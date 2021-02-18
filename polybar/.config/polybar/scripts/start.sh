267103807a5368f1cbb50fa10998556b5b93ea08
phrimm136 <39765074+phrimm136@users.noreply.github.com>
2021-02-18 13:31:15 +0900

    automatically find temp path

--- polybar/.config/polybar/scripts/start.sh
+++ polybar/.config/polybar/scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+export TEMP_PATH=/sys/devices/platform/coretemp.0/hwmon/$(ls /sys/devices/platform/coretemp.0/hwmon)/temp1_input
 killall polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 polybar top -c ~/.config/polybar/config-top.ini &
