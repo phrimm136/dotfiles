65343b78f02607a95ea372843827f4b854685da6
phrimm136 <39765074+phrimm136@users.noreply.github.com>
2021-02-20 10:22:40 +0900

    sync exported pdf and make current page persistent

--- emacs/.emacs.d/config/modes/init-pdf.el
+++ emacs/.emacs.d/config/modes/init-pdf.el
@@ -1,7 +1,15 @@
 ;;; init-pdf.el --- configuration for pdf format -*- lexical-binding: t -*-
 
 ;;; Commentary:
+
 ;;; Code:
+
+
+;;; sync pdf
+
+(push '(".*\\.pdf" . (nil (reusable-frames . t)
+                          (inhibit-switch-frame . t)))
+      display-buffer-alist)
 
 
 ;;; pdf viewer
@@ -10,6 +18,13 @@
   :straight t
   :mode (("\\.pdf\\'" . pdf-view-mode))
   :config (progn (evil-set-initial-state 'pdf-view-mode 'normal)))
+
+
+;;; persistent page
+
+(leaf saveplace-pdf-view
+  :straight t
+  :config (save-place-mode 1))
 
 
 ;;; keymap
