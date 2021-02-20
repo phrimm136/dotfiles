c673485824cc0329c9d29b05b2c36d6aa00fefee
phrimm136 <39765074+phrimm136@users.noreply.github.com>
2021-02-20 10:24:22 +0900

    drop lsp-ui

--- emacs/.emacs.d/config/utils/init-lsp.el
+++ emacs/.emacs.d/config/utils/init-lsp.el
@@ -23,35 +23,31 @@
 
 ;;; ui
 
-(leaf lsp-ui
-  :straight t
-  :hook ((lsp-mode-hook . lsp-ui-mode)
-         (lsp-ui-imenu-mode-hook . (lambda ()
-                                     (with-current-buffer "*lsp-ui-imenu*"
-                                       (setq-local face-remapping-alist
-                                                   `((default . (:background "#181818"))))))))
-  :setq ((lsp-ui-doc-enable . nil)
-         (lsp-ui-doc-header . nil)
-         (lsp-ui-doc-delay . 0.1)
-         (lsp-ui-doc-position . 'at-point)
-         (lsp-ui-doc-border . "black")
-         (lsp-ui-doc-use-childframe . t)
-         (lsp-ui-doc-include-signature . t)
-         (lsp-ui-peek-enable . t)
-         (lsp-ui-peek-always-show . nil)
-         (lsp-ui-peek-fontify . 'on-demand)
-         (lsp-ui-sideline-enable . t)
-         (lsp-ui-sideline-show-hover . t)
-         (lsp-ui-sideline-update-mode . 'line)
-         (lsp-ui-sideline-show-diagnostics . t)
-         (lsp-ui-sideline-display . 0.5)
-         (lsp-ui-flycheck-enable . t)
-         (lsp-ui-imenu-kind-position . 'left))
-  :bind ((:lsp-ui-peek-mode-map
-          ("C-j" . lsp-ui-peek--select-next)
-          ("C-k" . lsp-ui-peek--select-prev)
-          ("C-h" . lsp-ui-peek--select-prev-file)
-          ("C-l" . lsp-ui-peek--select-next-file))))
+;; (leaf lsp-ui
+;;   :straight t
+;;   :hook ((lsp-mode-hook . lsp-ui-mode))
+;;   :setq ((lsp-ui-doc-enable . nil)
+;;          (lsp-ui-doc-header . nil)
+;;          (lsp-ui-doc-delay . 0.1)
+;;          (lsp-ui-doc-position . 'at-point)
+;;          (lsp-ui-doc-border . "black")
+;;          (lsp-ui-doc-use-childframe . t)
+;;          (lsp-ui-doc-include-signature . t)
+;;          (lsp-ui-peek-enable . t)
+;;          (lsp-ui-peek-always-show . nil)
+;;          (lsp-ui-peek-fontify . 'on-demand)
+;;          (lsp-ui-sideline-enable . t)
+;;          (lsp-ui-sideline-show-hover . t)
+;;          (lsp-ui-sideline-update-mode . 'line)
+;;          (lsp-ui-sideline-show-diagnostics . t)
+;;          (lsp-ui-sideline-display . 0.5)
+;;          (lsp-ui-flycheck-enable . t)
+;;          (lsp-ui-imenu-kind-position . 'left))
+;;   :bind ((:lsp-ui-peek-mode-map
+;;           ("C-j" . lsp-ui-peek--select-next)
+;;           ("C-k" . lsp-ui-peek--select-prev)
+;;           ("C-h" . lsp-ui-peek--select-prev-file)
+;;           ("C-l" . lsp-ui-peek--select-next-file))))
 
 
 (leaf lsp-ivy
